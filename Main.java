10ee70afc9985bbf375f7a492ce2e0e296fb5c04
chenzeyu217230118 <161273360+chenzeyu217230118@users.noreply.github.com>
2024-03-04 22:36:34 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {public static void main(String[] args) throws Exception{
+        int[] array = {38,65,3,17,2};
+        BubbleSort bubblesort = new BubbleSort();
+        bubblesort.bubbleSort(array);
+    }
+}
